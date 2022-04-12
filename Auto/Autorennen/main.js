648a572312d00087ce85b6dab75fed9062d177af
HeleneElena <heleneebert1@gmail.com>
2022-04-12 15:22:48 +0200

    commit5

--- Auto/Autorennen/main.js
+++ Auto/Autorennen/main.js
@@ -44,6 +44,7 @@ function startGame() {
         enemy.y = -100 * setting.traffic * (i + 1);
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url("./image/enemy2.png") center / cover no-repeat';
         gameArea.append(enemy);
     }
 
@@ -102,7 +103,18 @@ function moveRoad() {
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left && 
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                setting.start = false;
+            console.warn('Unfall!!!');
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight) {
